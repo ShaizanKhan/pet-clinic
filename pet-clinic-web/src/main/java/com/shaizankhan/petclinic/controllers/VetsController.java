60cc6c2ff12148d235d5c7879f31c6d1ca072efa
Shaizan Khan <shaizan.khan@betsol.com>
2019-11-20 23:41:52 +0530

    Added vets list and updated controller. closes #15

--- pet-clinic-web/src/main/java/com/shaizankhan/petclinic/controllers/VetsController.java
+++ pet-clinic-web/src/main/java/com/shaizankhan/petclinic/controllers/VetsController.java
@@ -1,6 +1,8 @@
 package com.shaizankhan.petclinic.controllers;
 
+import com.shaizankhan.petclinic.services.VetService;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 /**
@@ -8,8 +10,16 @@ import org.springframework.web.bind.annotation.RequestMapping;
  */
 @Controller
 public class VetsController {
+
+    private VetService vetService;
+
+    public VetsController(VetService vetService) {
+        this.vetService = vetService;
+    }
+
     @RequestMapping({"/vets","/vets/index","/vets/index.html"})
-    public String getVetList(){
+    public String getVetList(Model model){
+        model.addAttribute("vets", vetService.findAll());
         return "/vets/index";
     }
 }
