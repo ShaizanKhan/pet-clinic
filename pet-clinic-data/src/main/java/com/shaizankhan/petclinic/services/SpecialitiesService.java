c126189854fbee3b143a712751d33f9620920ddc
Shaizan Khan <shaizan.khan@betsol.com>
2019-11-26 22:30:07 +0530

    added Specialities map impl service. closes #31

---
+++ pet-clinic-data/src/main/java/com/shaizankhan/petclinic/services/SpecialitiesService.java
@@ -0,0 +1,7 @@
+package com.shaizankhan.petclinic.services;
+
+import com.shaizankhan.petclinic.model.Speciality;
+
+public interface SpecialitiesService extends CrudService<Speciality, Long> {
+
+}
