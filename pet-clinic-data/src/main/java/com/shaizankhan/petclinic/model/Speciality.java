9f41db93b97f9fe1452ef1013d6c5647381c11d7
Shaizan Khan <shaizan.khan@betsol.com>
2019-11-27 22:52:06 +0530

    updated vet to use jpa mappings. closes #37

--- pet-clinic-data/src/main/java/com/shaizankhan/petclinic/model/Speciality.java
+++ pet-clinic-data/src/main/java/com/shaizankhan/petclinic/model/Speciality.java
@@ -1,7 +1,15 @@
 package com.shaizankhan.petclinic.model;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+
+/**
+ * @author Shaizan.Khan
+ */
+@Entity
 public class Speciality extends BaseEntity {
 
+    @Column(name = "description")
     private String description;
 
     public String getDescription() {
