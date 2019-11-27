c6b3fb4d57056a6a07b783323ff6bf8fdb3ad3cb
Shaizan Khan <shaizan.khan@betsol.com>
2019-11-27 21:24:12 +0530

    Updated base entity with JPA annotations. closes #35

--- pet-clinic-data/src/main/java/com/shaizankhan/petclinic/model/BaseEntity.java
+++ pet-clinic-data/src/main/java/com/shaizankhan/petclinic/model/BaseEntity.java
@@ -1,12 +1,19 @@
 package com.shaizankhan.petclinic.model;
 
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.MappedSuperclass;
 import java.io.Serializable;
 
 /**
  * @author Shaizan.Khan
  */
+@MappedSuperclass
 public class BaseEntity implements Serializable {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     public Long getId() {
