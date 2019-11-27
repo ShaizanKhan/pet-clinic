a8fafa0b37c36cd6bb488c665512db53c52f979b
Shaizan Khan <shaizan.khan@betsol.com>
2019-11-27 22:41:02 +0530

    updated owner with JPA entity mappings. closes #36

--- pet-clinic-data/src/main/java/com/shaizankhan/petclinic/model/PetType.java
+++ pet-clinic-data/src/main/java/com/shaizankhan/petclinic/model/PetType.java
@@ -1,7 +1,17 @@
 package com.shaizankhan.petclinic.model;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+/**
+ * @author Shaizan.Khan
+ */
+@Entity
+@Table(name = "types")
 public class PetType extends BaseEntity{
 
+    @Column(name = "name")
     private String name;
 
     public String getName() {
