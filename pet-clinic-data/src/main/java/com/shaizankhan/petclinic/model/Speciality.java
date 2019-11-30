7186e6d263cc12cf829c2ffa714efe38d1466a34
Shaizan Khan <shaizan.khan@betsol.com>
2019-12-01 00:14:37 +0530

    added project lombak. closes #51

--- pet-clinic-data/src/main/java/com/shaizankhan/petclinic/model/Speciality.java
+++ pet-clinic-data/src/main/java/com/shaizankhan/petclinic/model/Speciality.java
@@ -1,22 +1,22 @@
 package com.shaizankhan.petclinic.model;
 
+import lombok.*;
+
 import javax.persistence.Column;
 import javax.persistence.Entity;
 
 /**
  * @author Shaizan.Khan
  */
+@Setter
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 @Entity
 public class Speciality extends BaseEntity {
 
     @Column(name = "description")
     private String description;
 
-    public String getDescription() {
-        return description;
-    }
-
-    public void setDescription(String description) {
-        this.description = description;
-    }
 }
--- pet-clinic-data/src/main/java/com/shaizankhan/petclinic/model/Vet.java
+++ pet-clinic-data/src/main/java/com/shaizankhan/petclinic/model/Vet.java
@@ -1,5 +1,7 @@
 package com.shaizankhan.petclinic.model;
 
+import lombok.*;
+
 import javax.persistence.*;
 import java.util.HashSet;
 import java.util.Set;
@@ -7,6 +9,11 @@ import java.util.Set;
 /**
  * @author Shaizan.Khan
  */
+@Setter
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 @Entity
 @Table(name = "vets")
 public class Vet extends Person {
@@ -16,11 +23,4 @@ public class Vet extends Person {
                     inverseJoinColumns = @JoinColumn(name = "speciality_id"))
     private Set<Speciality> specialities = new HashSet<>();
 
-    public Set<Speciality> getSpecialities() {
-        return specialities;
-    }
-
-    public void setSpecialities(Set<Speciality> specialities) {
-        this.specialities = specialities;
-    }
 }
